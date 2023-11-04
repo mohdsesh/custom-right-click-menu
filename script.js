eaaf0ba0ac8c207957523a4490780d12fe6e502d
mohdsesh <144240089+mohdsesh@users.noreply.github.com>
2023-11-04 14:11:15 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const contextMenu = document.querySelector('.container');
+const subMenu = document.querySelector('.share-menu');
+
+document.addEventListener('contextmenu', (e)=>{
+    e.preventDefault()
+    let x = e.offsetX;
+    let y = e.offsetY;
+    let winWidth = window.innerWidth;
+    let winHeight = window.innerHeight;
+
+    let cmWidhth = contextMenu.offsetWidth
+    let cmHeight = contextMenu.offsetWidth
+
+x = x > winWidth - cmWidhth ? winWidth - cmWidhth : x 
+y = y > winHeight - cmHeight ? winHeight - cmHeight : y 
+
+if(x > (winWidth - cmHeight - subMenu.offsetWidth)){
+    subMenu.style.left = '-170px'
+}else{
+    subMenu.style.left = '260px'
+}
+
+    contextMenu.style.left = `${x}px`
+    contextMenu.style.top = `${y}px`
+    contextMenu.style.visibility = 'visible'
+});
+
+document.addEventListener('click', ()=>{
+    contextMenu.style.visibility = 'hidden'
+})
